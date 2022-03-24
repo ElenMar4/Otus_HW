0d752a0aa73314db5d05fd594e0a06523a913089
ElenMar4 <mar4enko.elena.jurjevna@gmail.com>
2022-03-24 07:45:49 +0300

    first load

---
+++ src/Testing/App/Demo.java
@@ -0,0 +1,13 @@
+package Testing.App;
+
+import Testing.App.Game.*;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Dice dice = new DiceImpl();
+        GameWinnerPrinter winnerPrinter = new GameWinnerConsolePrinter();
+        Game game = new Game(dice, winnerPrinter);
+        game.playGame(new Player("Вася"), new Player("Игорь"));
+    }
+}
---
+++ src/Testing/App/Game/GameWinnerPrinter.java
@@ -0,0 +1,5 @@
+package Testing.App.Game;
+
+public interface GameWinnerPrinter {
+    void printWinner(Player winner);
+}
