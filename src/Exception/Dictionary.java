41955d9c77d0d8b840169e368f74caf990230c55
ElenMar4 <mar4enko.elena.jurjevna@gmail.com>
2022-03-29 10:34:49 +0300

    original files

--- src/Exception/Dictionary.java
+++ src/Exception/Dictionary.java
@@ -1,5 +1,10 @@
 package Exception;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,15 +18,68 @@ public class Dictionary {
         return dictionary;
     }
 
-    public String getValue(String key) {
+    //Поиск в словаре перевода
+    public String[] getValue(String key) {
         Map<String, String> dictionary = createDictionary();
-        String value = null;
+        String[] value = {key, null};
         for (String count : dictionary.keySet()) {
             if (count.equals(key)) {
-                value = dictionary.get(key);
+                value[1] = dictionary.get(key);
+                break;
+            }
+        } return value;
+    }
+
+    //прверка результата поиска
+    public String messageOutput(String[] resultGetValue){
+        try {
+            if (resultGetValue[1] == null) {
+                newWords(resultGetValue[0]);
+                return "Translation isn't found\n";
+            } else System.out.println(resultGetValue[1] + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Enter word:";
+    }
+    //запись в файл слов, которых нет в словаре
+    public void newWords(String string) throws IOException {
+        String text = string +"\n";
+        try (FileOutputStream fos = new FileOutputStream("/home/alex/JB_2021_29/src/Exception/NewWords.txt", true)) {
+            // перевод строки в байты
+            byte[] buffer = text.getBytes();
+            fos.write(buffer, 0, buffer.length);
+            System.out.println("The file has been written");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    //Вывод списка слов, которых нет в словаре
+    public void printNewWords() {
+        FileReader reader = null;
+        try {
+            reader = new FileReader("/home/alex/JB_2021_29/src/Exception/NewWords.txt");
+            char[] buf = new char[256];
+            int count;
+            while ((count = reader.read(buf)) > 0) {
+                if (count < 256) {
+                    buf = Arrays.copyOf(buf, count);
+                }
+                System.out.println("New words:");
+                System.out.print(buf);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }finally {
+            if (reader!= null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        return value;
     }
 
 }
--- src/Exception/ExercisesExc.java
+++ src/Exception/ExercisesExc.java
@@ -6,66 +6,35 @@ import java.util.Scanner;
 
 public class ExercisesExc {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
 
-        FileOutputStream fos = new FileOutputStream("/home/alex/JB_2021_29/src/Exception/NewWords.txt");
-        System.out.println("Если захотите остановиться, введите - exit");
-        System.out.println("Enter word:");
+        System.out.println("This is a translator program. From English into Russian.\nTo exit the program, type - exit.\nlet's start!\nEnter word:");
         Scanner sc = new Scanner(System.in);
         String string = sc.nextLine();
         Dictionary dictionary = new Dictionary();
-
-        while (isExit(string)) {
-            try {
-                String translationWord = dictionary.getValue(string);
-                if (translationWord == null) {
-                    System.out.println("Word isn't found" + "\n");
-                    newWords(string, fos);
-                } else System.out.println(translationWord + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println("Enter word:");
-                string = sc.nextLine();
-            }
+        while (isExit(string)){
+            while (isLatin(string)){
+                System.out.println(dictionary.messageOutput(dictionary.getValue(string)));
+                break;
+            } string = sc.nextLine();
         }
-        printNewWords();
+        dictionary.printNewWords();
     }
 
-    //Проверка ввода латинских букв, и выход из программы
-    public static boolean isExit(String string) {
-
+    public static boolean isLatin(String string){
         if (string.matches("^[a-zA-Z0-9]+$")) {
-            if (string.equals("exit")) {
-                System.out.println("\n До встречи!");
-                System.out.println("\n Не забудь обновить словарь:");
-                return false;
-            } else {
-                return true;
-            }
-        } else throw new AlphabetEntryException("Input Error. Use latin letters.");
-    }
-
-    //запись в файл слов, которых нет в словаре
-    public static void newWords(String string, FileOutputStream fos) throws IOException {
-        PrintStream printStream = new PrintStream(fos);
-        printStream.println(string);
-        System.out.println("Запись в файл выполнена\n");
-    }
-
-    //чтение из файла, вывод новых слов для добавления в словарь
-    public static void printNewWords() {
-        try (FileReader reader = new FileReader("/home/alex/JB_2021_29/src/Exception/NewWords.txt")) {
-            char[] buf = new char[256];
-            int count;
-            while ((count = reader.read(buf)) > 0) {
-                if (count < 256) {
-                    buf = Arrays.copyOf(buf, count);
-                }
-                System.out.print(buf);
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            return true;
+        } else {
+            throw new AlphabetEntryException("Input Error. Use latin letters.");
         }
     }
+
+    public static boolean isExit(String string) {
+        if (string.equals("exit")) {
+            System.out.println("\n See you, bye!");
+            System.out.println("\n Don't forget to add new words to the dictionary:");
+            return false;
+        } else return true;
+    }
+
 }
