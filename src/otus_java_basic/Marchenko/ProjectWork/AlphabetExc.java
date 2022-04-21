cf973b8f3ced4d8733dad6b57a34b211fd9aa29d
ElenMar4 <mar4enko.elena.jurjevna@gmail.com>
2022-04-21 12:01:22 +0300

    first loading

---
+++ src/otus_java_basic/Marchenko/ProjectWork/AlphabetExc.java
@@ -0,0 +1,8 @@
+package otus_java_basic.Marchenko.ProjectWork;
+
+public class AlphabetExc extends RuntimeException {
+
+        public AlphabetExc(String massage) {
+            super(massage);
+        }
+}
